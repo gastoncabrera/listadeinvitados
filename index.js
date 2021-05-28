f4b7a03175b06d586dfd08d9d69cf90164fae372
Cabrera Gaston <cabreragastonivan31@gmail.com>
2021-05-27 23:46:42 -0300

    pasando a pug

--- index.js
+++ index.js
@@ -1,84 +1,80 @@
 // Selectores
-let formulario = document.getElementById("form");
-let inputNamber = document.getElementById("number");
-let btn = document.querySelector("#btn");
-let invitedList = document.querySelector("#invitado");
-let invitedName = document.querySelector("#invitado__name");
+const formEl = document.getElementById("form");
+const maxInvitedEl = document.getElementById("number");
+const errorNumber = document.querySelector(".errornumber");
+const errorAmount = document.querySelector(".erroramount");
 
 // arrays de los invitados y la cantidad
-let invitados = [];
-let number = { number: 0 };
+const invitados = [];
+let maxInvited = 0;
 
-// funcion para validar el formulario
-function validation(formData) {
-  try {
-    if (formData.get("username") === null || formData.get("username") === "") {
-      throw Error;
-    } else {
-      invitados.push(formData.get("username"));
-    }
-  } catch {
-    alert("debes colocar un nombre");
-  }
-}
-// formulario
-formulario.addEventListener("submit", (event) => {
+// formEl
+formEl.addEventListener("submit", (event) => {
   event.preventDefault();
-  formData = new FormData(formulario);
-  validation(formData);
-  // establecerLimite(number);
-  // number.splice(number[number.length - 1], 1);
-  // myFunction(invitados, number);
-  // formulario.reset();
+  const formData = new FormData(formEl);
+
+  // if (isValidateName(formData)) {
+  //   if (canCreateInvited()) {
+  //     invitados.push(formData.get("username"));
+  //     formEl.reset();
+  //     createInvited();
+  //   } else {
+  //     alert("Exediste el numero de invitados establecido");
+  //   }
+  // } else {
+  //   alert("debes colocar un nombre");
+  // }
+
+  if (!isValidateName(formData)) {
+    errorAmount.innerHTML = "Debes colocar un nombre";
+    return;
+  }
+
+  if (!canCreateInvited()) {
+    errorNumber.innerHTML = "Exediste el numero de invitados establecido";
+    return;
+  }
+
+  invitados.push(formData.get("username"));
+  formEl.reset();
+  createInvited();
 });
 
-function establecerLimite() {
-  let numbernew = inputNamber.value;
-  return (number = numbernew);
+maxInvitedEl.addEventListener("input", () => {
+  maxInvited = event.target.value || 0;
+});
+
+// funcion para validar el formEl
+function isValidateName(formData) {
+  if (formData.get("username") === null || formData.get("username") === "") {
+    return false;
+  } else {
+    return true;
+  }
 }
-// function myFunction(invitados, number) {
-//   try {
-//     if (invitados.length <= number[number.length - 1]) {
-//       createInvited();
-//     } else {
-//       throw new Error();
-//     }
-//   } catch {
-//     console.log("error");
-//     alert("Exediste el numero de invitados establecido");
-//   }
-// }
 
-// function createInvited(validationForm) {
-//   if (validationForm() === true) {
-//     var li = document.createElement("LI");
-//     var p = document.createElement("P");
-//     var button = document.createElement("BUTTON");
-//     var nameInvitado = document.createTextNode(invitados[invitados.length - 1]);
-//     var textdelete = document.createTextNode("X");
-//     button.appendChild(textdelete);
-//     button.classList.add("delete");
-//     p.appendChild(nameInvitado);
-//     li.appendChild(p).appendChild(button);
-//     document
-//       .getElementById("prueba")
-//       .appendChild(li)
-//       .classList.add("capitalize");
-//     button.addEventListener("click", (event) =>
-//       console.log(event.target.parentNode.parentNode.remove())
-//     );
-//   } else {
-//     console.log("error");
-//   }
-// }
+function canCreateInvited() {
+  if (invitados.length < maxInvited) {
+    return true;
+  } else {
+    return false;
+  }
+}
 
-// node.setAttribute("Id", 1);
-// if (
-//   invitados[invitados.length - 1] === null ||
-//   invitados[invitados.length - 1] === ""
-// ) {
-//   invitados.splice(invitados[invitados.length - 1], 1);
-//   invitados.splice(number[number.length - 2], 1);
-//   alert("Es necesario poner el nombre");
-//   return;
-// }
+function createInvited() {
+  const li = document.createElement("LI");
+  const p = document.createElement("P");
+  const button = document.createElement("BUTTON");
+  const nameInvitado = document.createTextNode(invitados[invitados.length - 1]);
+  const textdelete = document.createTextNode("X");
+
+  button.appendChild(textdelete);
+  button.classList.add("delete");
+  p.appendChild(nameInvitado);
+  li.appendChild(p).appendChild(button);
+  document.getElementById("prueba").appendChild(li).classList.add("capitalize");
+
+  button.addEventListener("click", (event) =>
+    event.target.parentNode.parentNode.remove()
+  );
+}
